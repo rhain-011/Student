3518cff1b07bf92dae82b2bf6af564d961e6d188
Rhainel <rhainelperalta@gmail.com>
2021-01-19 13:19:46 -0500

    add email

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -30,6 +30,27 @@ public class Student {
     public void setName(String name) {
         this.name = name;
     }
+    
+    private String email;
+
+    /**
+     * Get the value of email
+     *
+     * @return the value of email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @param email new value of email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 
 }
  
