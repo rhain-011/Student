7dcc95a64b81f6f5604af15f7a27eddcbf0ca30d
rhain-011 <36254247+rhain-011@users.noreply.github.com>
2021-01-26 12:33:43 -0500

    Update Student.java

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ca.sheridancollege.week1.softwarefundamentals;
 
 /**
  *
- * @author asidhu
- */
+ * @author asidhu - 01-26-2021
+ */ 
 public class Student {
     
     private String name;
